0693fe4f128eaccf1ab9ddb596b2a8271542f676
Rob Wilson <rob.wilson@salesloft.com>
2017-10-04 11:47:43 -0400

    Adds Docker aliases, corteousy of @GrantJamesPowell

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 1. Create sym link for `.vimrc` in `~/`.
 1. Copy `my-paths` to `/etc/paths.d` (note: cannot symlink to this file).
 1. Create sym link for `.gitconfig` in `~`.
+1. Create sym link for `.ssh/config` in `~/.ssh`.
---
+++ bash_settings/docker.sh
@@ -0,0 +1,17 @@
+alias dcd="docker-compose down"
+alias dcu="docker-compose up -d"
+alias dcub="docker-compose up --build -d"
+
+# see all running docker processes
+alias dps="docker ps"
+
+# get logs
+alias dl="docker-compose logs"
+
+# down clean and up
+alias dsh="docker-compose down && docker system prune -f && docker-compose up --build -d"
+alias di="docker image"
+
+# shell into a pod by doing `de {first_three letters of sha}`
+docker_exec(){ docker exec -ti $(docker ps | grep $1 |cut -d' ' -f 1) bash; }
+alias de='docker_exec'
