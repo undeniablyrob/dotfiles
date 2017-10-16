bfee0745fc86a2342d503a2eb20b9a396ace8981
Rob Wilson <rob.wilson@salesloft.com>
2017-10-16 15:38:25 -0400

    Adds git pull alias

--- bash_settings/git.sh
+++ bash_settings/git.sh
@@ -1,6 +1,7 @@
 alias gs='git status'
 alias gd='git diff'
 alias gb='git branch'
+alias gp='git pull'
 alias gcm='git checkout master'
 alias gcs='git checkout -'
 alias gpom='git pull origin master'
