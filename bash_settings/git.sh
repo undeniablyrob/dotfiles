50fd56faa8edd79751a4efe626d5050d76eaa987
Rob Wilson <rob.wilson@salesloft.com>
2018-02-12 19:44:21 -0500

    Adds a custom branch list formatter

--- bash_settings/git.sh
+++ bash_settings/git.sh
@@ -1,6 +1,6 @@
 alias gs='git status'
 alias gd='git diff'
-alias gb='git branch'
+alias gb='gitbranchformat'
 alias gp='git pull'
 alias gcm='git checkout master'
 alias gcs='git checkout -'
@@ -8,8 +8,8 @@ alias gxp='git cherry-pick'
 alias gpom='git pull origin master'
 alias gl='git lg'
 alias gml='git mylg'
-
-alias listbranch="echo $(parse_git_branch)"
+alias listbranch='echo "$(parse_git_branch)"'
+alias gitbranchformat="git branch --sort=committerdate --format='%(HEAD) %(color:white)%(refname:short)%(color:reset) - %(color:green)%(committerdate:relative) %(color:cyan dim)%(authorname) %(color:reset)'"
 
 gitfiles () {
   local commit_hash=$1;
