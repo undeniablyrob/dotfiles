9f9b349e93f0b4902dac6e0e600fc0ae147d451d
Rob Wilson <rob.wilson@salesloft.com>
2018-02-27 19:48:19 -0500

    Remove aliases that I just never remember

--- bash_settings/ruby_on_rails_dev.sh
+++ bash_settings/ruby_on_rails_dev.sh
@@ -1,10 +1,6 @@
 #------------- Ruby/Rails ---------------
 alias be="bundle exec"
 alias parl=parallel_rake_spec
-alias rntest=RAILS_ENV=test
-alias rndev=RAILS_ENV=development
-alias rakemigratetest='rake db:migrate RAILS_ENV=test'
-alias railsdbtest='RAILS_ENV=test rails db'
 
 parallel_rake_spec () {
   local path=$1
